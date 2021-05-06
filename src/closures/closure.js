708cb176439292cd71e91caf53a297bdc91d7dce
David Giraldo <davidgiraldobueno@gmail.com>
2021-05-06 19:10:37 -0400

    closure

---
+++ src/closures/closure.js
@@ -0,0 +1,25 @@
+// No closure
+const moneyBox = (coins) => {
+  var saveCoins = 0;
+  saveCoins += coins;
+  console.log(`Money box: $${saveCoins}`);
+};
+
+moneyBox(5);
+moneyBox(10);
+
+
+
+const moneyBox = () => {
+  var saveCoins = 0;
+  const countCoins = (coins) => {
+    saveCoins += coins;
+    console.log(`Money box: $${saveCoins}`);
+  };
+  return countCoins;
+};
+
+let myMoneyBox = moneyBox();
+myMoneyBox(4);
+myMoneyBox(6);
+myMoneyBox(10);
