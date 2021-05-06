6f1c08cdaf358d5725816eaccf0d72bd84697641
David Giraldo <davidgiraldobueno@gmail.com>
2021-05-06 12:08:57 -0400

    global scope

---
+++ src/scope/global.js
@@ -0,0 +1,15 @@
+var hello = "Hello";
+var hello = "Hello 2";
+
+let world = "world!";
+
+const helloWorld = "Hello World!";
+
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(helloWorld);
+}
+
+anotherFunction();
