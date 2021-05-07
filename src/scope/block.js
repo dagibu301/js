abd60b52af99c7919ef4425923a9746cd613f4b7
David Giraldo <davidgiraldobueno@gmail.com>
2021-05-06 20:29:47 -0400

    closures

--- src/scope/block.js
+++ src/scope/block.js
@@ -10,3 +10,14 @@ const fruits = () => {
 };
 
 fruits();
+
+
+const anotherFunction = () => {
+    for (let i = 0; i < 10; i++) {
+      setTimeout(() => {
+        console.log(i);
+      }, 1000);
+    }
+  };
+  
+  anotherFunction();
